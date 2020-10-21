7944e8909ad88da6e46bfccaf2f3a498a36313d7
Kirby Wu <tkirby@gmail.com>
2020-10-21 13:01:39 +0800

    add flagsihp

---
+++ web/.view/flagship/user/index.js
@@ -0,0 +1,16 @@
+ (function() { function pug_attr(t,e,n,f){return!1!==e&&null!=e&&(e||"class"!==t&&"style"!==t)?!0===e?" "+(f?t:t+'="'+t+'"'):("function"==typeof e.toJSON&&(e=e.toJSON()),"string"==typeof e||(e=JSON.stringify(e),n||-1===e.indexOf('"'))?(n&&(e=pug_escape(e))," "+t+'="'+e+'"'):" "+t+"='"+e.replace(/'/g,"&#39;")+"'"):""}
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (url) {;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Ftkirby\u002Fworkspace\u002Fzbryikt\u002Fletas\u002Fweb\u002Fsrc\u002Fpug\u002Fbase.pug";
+pug_html = pug_html + "\u003C!DOCTYPE html\u003E";
+;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Ftkirby\u002Fworkspace\u002Fzbryikt\u002Fletas\u002Fweb\u002Fsrc\u002Fpug\u002Fbase.pug";
+pug_html = pug_html + "\u003Chtml\u003E";
+;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Ftkirby\u002Fworkspace\u002Fzbryikt\u002Fletas\u002Fweb\u002Fsrc\u002Fpug\u002Fbase.pug";
+;pug_debug_line = 3;pug_debug_filename = "src\u002Fpug\u002Fflagship\u002Fuser\u002Findex.pug";
+url = "https://dash.taicca.tw/dash/brd/flagship-2/grp/4QfoCM02LC05ZK0B_z02plB-x/judge/custom/2T+u6H0sy05lt0AFm04qpWyr/user";
+;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Ftkirby\u002Fworkspace\u002Fzbryikt\u002Fletas\u002Fweb\u002Fsrc\u002Fpug\u002Fbase.pug";
+pug_html = pug_html + "\u003Chead\u003E";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Ftkirby\u002Fworkspace\u002Fzbryikt\u002Fletas\u002Fweb\u002Fsrc\u002Fpug\u002Fbase.pug";
+pug_html = pug_html + "\u003Cmeta" + (" http-equiv=\"refresh\""+pug_attr("content", `0; URL=${url}`, true, true)) + "\u003E\u003C\u002Fhead\u003E";
+;pug_debug_line = 7;pug_debug_filename = "\u002FUsers\u002Ftkirby\u002Fworkspace\u002Fzbryikt\u002Fletas\u002Fweb\u002Fsrc\u002Fpug\u002Fbase.pug";
+pug_html = pug_html + "\u003Cbody\u003E\u003C\u002Fbody\u003E\u003C\u002Fhtml\u003E";}.call(this,"url" in locals_for_with?locals_for_with.url:typeof url!=="undefined"?url:undefined));} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line);};return pug_html;}; module.exports = template; })() 
